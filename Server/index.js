41c015777553390e81afb8eb7cfd7375ed1bd434
anna cole <kozlowsa1@hawkmail.newpaltz.edu>
2017-10-12 11:14:43 -0400

    update

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,16 @@
 const http = require("express");
-const _ = require("lodash");
-const handler = require("./httpHandler.js");
+const handler = require("./httpHandler.js")
 
 const server = http();
 
 server.use("/json", function(req, res, next){
-    res.send({happy: "Yom Kippur"})
+    res.send({ happy: "Yom Kippur"})
     next();
 });
+
 server.use("/old", handler.main);
+    
+
 server.listen(3000);
 
 console.log("http://localhost:3000");
