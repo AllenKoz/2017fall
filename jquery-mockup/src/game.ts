52f4e0f30933d310e75efab0cca80e28a75e25bb
anna cole <kozlowsa1@hawkmail.newpaltz.edu>
2017-10-26 12:16:42 -0400

    pictures change

--- jquery-mockup/src/game.ts
+++ jquery-mockup/src/game.ts
@@ -35,6 +35,19 @@ export class Room {
             this.players.map( x => `<li class="list-group-item">${x.name}</li>` ).join("")
         );
     }
+    update(){
+        $.get("/game/room/picture").done( data => {
+            this.picture = data;
+            this.drawPicture();
+        }),
+        $.get("/game/room/quotes").done( data =>{
+            this.quotes = data;
+            this.drawQuotes();
+        });
+    }
+    init(){
+        setInterval(() => this.update(), 1000)
+    }
 }
 
 export class Game {
@@ -58,12 +71,9 @@ export class Game {
 const game = new Game();
 const room = new Room();
 const me = new Player();
-var i = 0;
 
+room.init();
 game.init().done(()=>{
-    room.picture = game.pictures[i];
-    room.drawPicture();
-    room.drawQuotes();
     room.drawPlayers();
 
     me.quotes = game.quotes;
@@ -74,7 +84,5 @@ game.init().done(()=>{
 
 $("#cmd-flip").click(function(e){
     e.preventDefault();
-    i++;
-    room.picture = game.pictures[i];
-    room.drawPicture();
+    $.post("/game/room/picture")
 })
