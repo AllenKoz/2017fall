b64207679cb1cdf2ac9f1b1263b3a7343b2a4f80
anna cole <kozlowsa1@hawkmail.newpaltz.edu>
2017-11-09 12:15:42 -0500

    Play.html, game controller.js, index.js ,play components.js,game.ts

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -12,24 +12,37 @@ export class PlayComponent implements OnInit {
 
   apiRoot = "//localhost:3001"
   room = new Room();
-
+  me = new Player();
   constructor() { }
 
   ngOnInit() {
     setInterval(()=> this.update(), 1000)
+    $.getJSON(this.apiRoot + "/game/quotes").done( data =>{
+      this.me.quotes = data;
+  })
     
   }
 
   update(){
     $.get(this.apiRoot + "/game/room/picture").done( data => {
         this.room.picture = data;
-        this.room.drawPicture();
+
     });
     $.getJSON(this.apiRoot + "/game/room/quotes").done( data =>{
         this.room.quotes = data;
-        this.room.drawQuotes();
+
     });
 }
+flipPicture(e: MouseEvent){
+  e.preventDefault();
+  $.post(this.apiRoot + "/game/room/picture")
+}
 
+submitQuote(e: MouseEvent, quote: Quote, i: number){
+  e.preventDefault();
+  const data = { text: quote.text };
+  $.post(this.apiRoot + "/game/room/quotes", data);
+  this.me.quotes.splice(i, 1);
+}
 
 }
